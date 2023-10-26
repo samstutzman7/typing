5adc990b5cdb70247e187d6002cf87598a58711c
samstutzman7 <sam.stutz7@hotmail.com>
2023-10-26 13:01:43 -0500

    Initial Setup

---
+++ typing-test/src/components/SpeedTypingGame.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import './SpeedTypingGame.css';
+import TypingArea from './TypingArea';
+
+const SpeedTypingGame = () => {
+    const paragraphs = [
+       "A plant is one of the most important living things that develop on the earth and is made up of stems, leaves, roots, and so on. Parts of the Plants: The part of the plant that developed beneath the soil is referred to as root and the part that grows outside of the soil is known as the shoot. The shoot consists of stems, branches, leaves, flowers, fruits, and seeds.",
+       "MORE PARAGRAPHS"
+    ]
+}
