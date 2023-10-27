5d48a35d777d546bbed059159bbb2ef408d41801
samstutzman7 <sam.stutz7@hotmail.com>
2023-10-26 23:23:44 -0500

    FINISH THE PARAGRAPHS
    Just finish the paragraphs and you are done!!!

--- typing-test/src/components/TypingArea.js
+++ typing-test/src/components/TypingArea.js
@@ -14,7 +14,7 @@ const TypingArea = ({
     return (
         <div className="section">
             <div className="text-container">
-                <p id="paragraph">{typingText}</p>
+                <p>{typingText}</p>
             </div>
             <div className="section2">
                 <ul className="resultDetails">
